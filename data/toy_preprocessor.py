4166607faad053b9895e9380d2689609e4a97fc1
Weilando <Hendryk.Weiland@hhu.de>
2022-04-28 18:48:24 +0200

    feat: extend TOY corpus, improve tests

--- data/toy_preprocessor.py
+++ data/toy_preprocessor.py
@@ -1,35 +1,58 @@
+from typing import List
+
 import pandas as pd
 
 from data.corpus_handler import CorpusName
 from data.corpus_preprocessor import CorpusPreprocessor
 from grimm_bert import DEFAULT_CORPUS_CACHE_DIR
 
-SENTENCES = [['She', 'glances', 'at', 'her', 'watch', '.'],
-             ['He', 'wants', 'to', 'watch', 'the', 'soccer', 'match', '.'],
-             ['We', 'watch', 'movies', 'and', 'eat', 'chips', '.'],
-             ['The', 'watch', 'should', 'match', 'her', 'coat', '.'],
-             ['The', 'new', 'chips', 'are', 'twice', 'as', 'fast', '.'],
-             ['She', 'wears', 'a', 'coat', 'to', 'keep', 'warm', '.'],
-             ['He', 'earned', 'many', 'chips', 'in', 'this', 'game', '.'],
-             ['He', 'earned', 'angry', 'glances', '.']]
+SENTENCES = [
+    ['she', 'glances', 'at', 'her', 'watch', '.'],
+    ['we', 'want', 'to', 'watch', 'some', 'pool', 'matches', '.'],
+    ['we', 'watch', 'movies', 'and', 'eat', 'chips', '.'],
+    ['can', 'you', 'please', 'watch', 'the', 'kids', '?'],
+    ['the', 'scarf', 'matches', 'her', 'coat', '.'],
+    ['the', 'last', 'chips', 'read', 'twice', 'as', 'fast', '.'],
+    ['she', 'wears', 'a', 'warm', 'coat', 'even', 'in', 'may', '.'],
+    ['he', 'earned', 'some', 'warm', 'glances', '.'],
+    ['some', 'earned', 'money', 'with', 'cold', 'soda', '.'],
+    ['can', 'we', 'book', 'a', 'hotel', 'with', 'a', 'pool', '?'],
+    ['do', 'you', 'read', 'her', 'last', 'book', '?'],
+    ['may', 'he', 'drink', 'the', 'last', 'can', 'of', 'soda', '?'],
+    ['he', 'wears', 'a', 'scarf', 'as', 'he', 'has', 'a', 'cold', '.'],
+    ['we', 'fast', 'soda', 'and', 'chips', '.'],
+    ['are', 'we', 'even', '?'],
+    ['we', 'can', 'eat', 'some', 'chips', 'at', 'the', 'movies', '.'],
+    ['the', 'book', 'is', 'a', 'fun', 'read', '.'],
+    ['the', 'kids', 'want', 'some', 'fun', 'in', 'the', 'pool', '.'],
+    ['may', 'we', 'order', 'a', 'drink', '?'],
+    ['please', 'watch', 'the', 'movies', 'in', 'order', '.']]
 
-TOKENS = ['she', 'glances', 'at', 'her', 'watch', '.',
-          'he', 'wants', 'to', 'watch', 'the', 'soccer', 'match', '.',
-          'we', 'watch', 'movies', 'and', 'eat', 'chips', '.',
-          'the', 'watch', 'should', 'match', 'her', 'coat', '.',
-          'the', 'new', 'chips', 'are', 'twice', 'as', 'fast', '.',
-          'she', 'wears', 'a', 'coat', 'to', 'keep', 'warm', '.',
-          'he', 'earned', 'many', 'chips', 'in', 'this', 'game', '.',
-          'he', 'earned', 'angry', 'glances', '.']
+SENSES = [
+    'she0', 'glances0', 'at0', 'her0', 'watch0', '.0',
+    'we0', 'want0', 'to0', 'watch1', 'some0', 'pool0', 'matches0', '.0',
+    'we0', 'watch1', 'movies0', 'and0', 'eat0', 'chips0', '.0',
+    'can0', 'you0', 'please0', 'watch2', 'the0', 'kids0', '?0',
+    'the0', 'scarf0', 'matches1', 'her0', 'coat0', '.0',
+    'the0', 'last0', 'chips1', 'read0', 'twice0', 'as0', 'fast0', '.0',
+    'she0', 'wears0', 'a0', 'warm0', 'coat0', 'even0', 'in0', 'may0', '.0',
+    'he0', 'earned0', 'some0', 'warm1', 'glances1', '.0',
+    'some1', 'earned1', 'money0', 'with0', 'cold0', 'soda0', '.',
+    'can0', 'we0', 'book0', 'a0', 'hotel0', 'with1', 'a0', 'pool1', '?0',
+    'do0', 'you0', 'read1', 'her0', 'last0', 'book1', '?0',
+    'may1', 'he0', 'drink0', 'the0', 'last1', 'can1', 'of0', 'soda0', '?0',
+    'he0', 'wears0', 'a0', 'scarf0', 'as1', 'he0', 'has0', 'a0', 'cold1', '.0',
+    'we0', 'fast1', 'soda0', 'and0', 'chips0', '.0',
+    'are0', 'we0', 'even1', '?0',
+    'we0', 'can0', 'eat0', 'some0', 'chips0', 'at1', 'the0', 'movies1', '.0',
+    'the0', 'book1', 'is0', 'a0', 'fun0', 'read2', '.0',
+    'the0', 'kids0', 'want0', 'some0', 'fun1', 'in1', 'the0', 'pool1', '.0',
+    'may1', 'we0', 'order0', 'a0', 'drink1', '?0',
+    'please0', 'watch1', 'the0', 'movies0', 'in2', 'order1', '.0']
 
-SENSES = ['she0', 'glances0', 'at0', 'her0', 'watch0', '.0',
-          'he0', 'wants0', 'to0', 'watch1', 'the0', 'soccer0', 'match0', '.0',
-          'we0', 'watch1', 'movies0', 'and0', 'eat0', 'chips0', '.0',
-          'the0', 'watch0', 'should0', 'match1', 'her0', 'coat0', '.0',
-          'the0', 'new0', 'chips1', 'are0', 'twice0', 'as0', 'fast0', '.0',
-          'she0', 'wears0', 'a0', 'coat0', 'to0', 'keep0', 'warm0', '.0',
-          'he0', 'earned0', 'many0', 'chips3', 'in0', 'this0', 'game0', '.0',
-          'he0', 'earned1', 'angry0', 'glances1', '.0']
+
+def flatten_list(nested_list: List[List[str]]) -> List[str]:
+    return sum(nested_list, [])
 
 
 class ToyPreprocessor(CorpusPreprocessor):
@@ -41,7 +64,8 @@ class ToyPreprocessor(CorpusPreprocessor):
         return pd.DataFrame({'sentence': SENTENCES})
 
     def get_tagged_tokens(self) -> pd.DataFrame:
-        return pd.DataFrame({'token': TOKENS, 'sense': SENSES})
+        return pd.DataFrame({'token': flatten_list(SENTENCES),
+                             'sense': SENSES})
 
 
 if __name__ == '__main__':
--- test/test_corpus_handler.py
+++ test/test_corpus_handler.py
@@ -23,8 +23,14 @@ class TestCorpusName(TestCase):
         self.assertTrue(CorpusName.SEMEVAL15.is_wsdeval_name)
 
 
+class TestToyPreprocessor(TestCase):
+    def test_flatten_list(self):
+        nested_list = [['a', 'b'], ['c']]
+        self.assertEqual(['a', 'b', 'c'], tp.flatten_list(nested_list))
+
+
 class TestCorpusHandler(TestCase):
-    def test_set_up_toy(self):
+    def test_set_up_toy_corpus_handler(self):
         with TemporaryDirectory() as tmp_dir_name:
             corpus = CorpusHandler(CorpusName.TOY, tmp_dir_name)
             self.assertEqual(CorpusName.TOY, corpus.corpus_name)
@@ -33,31 +39,18 @@ class TestCorpusHandler(TestCase):
             self.assertEqual("toy-tagged_tokens.pkl",
                              corpus.tagged_tokens_name)
 
-    def test_get_sentences(self):
-        with TemporaryDirectory() as tmp_dir:
-            toy_preprocessor = tp.ToyPreprocessor(corpus_cache_path=tmp_dir)
-            toy_preprocessor.cache_dataset()
-
-            expected = pd.DataFrame({'sentence': tp.SENTENCES})
-            corpus = CorpusHandler(CorpusName.TOY, tmp_dir)
-            pd.testing.assert_frame_equal(expected, corpus.get_sentences())
-
-    def test_get_sentences_as_list(self):
+    def test_access_toy_via_corpus_handler(self):
         with TemporaryDirectory() as tmp_dir:
             toy_preprocessor = tp.ToyPreprocessor(corpus_cache_path=tmp_dir)
             toy_preprocessor.cache_dataset()
 
             corpus = CorpusHandler(CorpusName.TOY, tmp_dir)
+
+            pd.testing.assert_frame_equal(toy_preprocessor.get_sentences(),
+                                          corpus.get_sentences())
             self.assertEqual(tp.SENTENCES, corpus.get_sentences_as_list())
-
-    def test_get_tagged_tokens(self):
-        with TemporaryDirectory() as tmp_dir:
-            toy_preprocessor = tp.ToyPreprocessor(corpus_cache_path=tmp_dir)
-            toy_preprocessor.cache_dataset()
-
-            expected = pd.DataFrame({'token': tp.TOKENS, 'sense': tp.SENSES})
-            corpus = CorpusHandler(CorpusName.TOY, tmp_dir)
-            pd.testing.assert_frame_equal(expected, corpus.get_tagged_tokens())
+            pd.testing.assert_frame_equal(toy_preprocessor.get_tagged_tokens(),
+                                          corpus.get_tagged_tokens())
 
 
 if __name__ == '__main__':
